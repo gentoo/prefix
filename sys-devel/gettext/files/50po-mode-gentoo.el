cc0185a54e413589cded681a8aedf54bbb146646
Kito Danya Dietrich <kito@gentoo.org>
2005-12-13 20:55:14 +0000

    drop -overlay

--- sys-devel/gettext/files/50po-mode-gentoo.el
+++
@@ -1,8 +0,0 @@
-
-;; site-file configuration for po-mode
-
-;; po-mode
-(setq auto-mode-alist (append '(("\\.po$" . po-mode)) auto-mode-alist))
-
-(autoload 'po-mode "po-mode" ".po file editting mode" t)
-
