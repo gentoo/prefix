fba3a152e16fbd9cc45639ac8ab48a13d80053ae
Fabian Groffen <grobian@gentoo.org>
2006-10-20 15:56:20 +0000

    use portage without collision-protect issues

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -161,7 +161,7 @@ bootstrap_tree() {
 bootstrap_portage() {
 	# don't use "latest" here, as I want to have the bootstrap script to
 	# use a portage in a known "state"
-	PV=2.1.20.4765
+	PV=2.1.20.4768
 	A=prefix-portage-${PV}.tar.bz2
 	einfo "Bootstrapping ${A%-*}"
 		
