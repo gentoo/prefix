9432b82a5a5d922df6b1cf842970ed7f554c47ec
Fabian Groffen <grobian@gentoo.org>
2007-04-05 10:56:13 +0000

    auto sync net-misc/rsync (Portage 2.1.22.6320-prefix/SVN/Darwin 8.9.0 powerpc)

--- net-misc/rsync/files/rsyncd.init.d
+++ net-misc/rsync/files/rsyncd.init.d
@@ -1,7 +1,7 @@
 #!@GENTOO_PORTAGE_EPREFIX@/sbin/runscript
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-misc/rsync/files/rsyncd.init.d,v 1.4 2004/10/21 01:10:48 vapier Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-misc/rsync/files/rsyncd.init.d,v 1.5 2007/02/23 11:33:59 uberlord Exp $
 
 depend() {
 	use net
@@ -9,12 +9,15 @@ depend() {
 
 start() {
 	ebegin "Starting rsyncd"
-	rsync --daemon ${RSYNC_OPTS}
+	start-stop-daemon --start --exec @GENTOO_PORTAGE_EPREFIX@/usr/bin/rsync \
+		--pidfile @GENTOO_PORTAGE_EPREFIX@/var/run/rsyncd.pid \
+		-- --daemon ${RSYNC_OPTS}
 	eend $?
 }
 
 stop() {
 	ebegin "Stopping rsyncd"
-	start-stop-daemon --stop --pidfile @GENTOO_PORTAGE_EPREFIX@/var/run/rsyncd.pid
+	start-stop-daemon --stop --exec @GENTOO_PORTAGE_EPREFIX@/usr/bin/rsync \
+		--pidfile @GENTOO_PORTAGE_EPREFIX@/var/run/rsyncd.pid
 	eend $?
 }
--- net-misc/rsync/rsync-2.6.9-r2.ebuild
+++ net-misc/rsync/rsync-2.6.9-r2.ebuild
@@ -1,6 +1,6 @@
 # Copyright 1999-2007 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-misc/rsync/rsync-2.6.9-r2.ebuild,v 1.1 2007/02/03 21:35:15 vapier Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-misc/rsync/rsync-2.6.9-r2.ebuild,v 1.2 2007/03/03 06:05:44 vapier Exp $
 
 EAPI="prefix"
 
@@ -15,10 +15,8 @@ SLOT="0"
 KEYWORDS="~amd64 ~ppc-aix ~ppc-macos ~sparc-solaris ~x86 ~x86-macos ~x86-solaris"
 IUSE="acl ipv6 static xinetd"
 
-RDEPEND=">=dev-libs/popt-1.5
+DEPEND=">=dev-libs/popt-1.5
 	acl? ( kernel_linux? ( sys-apps/acl ) )"
-DEPEND="${RDEPEND}
-	>=sys-apps/portage-2.0.51"
 
 S=${WORKDIR}/${P/_/}
 
