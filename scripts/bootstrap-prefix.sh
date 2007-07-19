41e602f17ba38a4030ef1184ee6bf8c49b5aa1f9
Fabian Groffen <grobian@gentoo.org>
2007-07-19 19:28:17 +0000

    Use an up2date snapshot, such that we don't downgrade portage again...

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -157,7 +157,7 @@ bootstrap_setup() {
 }
 
 bootstrap_tree() {
-	PV="20070628"
+	PV="20070719"
 	for x in etc usr/{,s}bin var/tmp var/lib/portage var/log/portage var/db;
 	do
 		[ -d "${ROOT}/${x}" ] || mkdir -p "${ROOT}/${x}"
