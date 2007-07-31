734fe475165294824915fafa198fb16448ef8cbf
Fabian Groffen <grobian@gentoo.org>
2007-07-31 09:16:18 +0000

    The package name should be enough for everyone

--- scripts/treesync/walker.sh
+++ scripts/treesync/walker.sh
@@ -4,17 +4,17 @@ export PTREEDIR="$(pwd -P)"
 for d in *-*/* virtual/* eclass licenses profiles/base profiles/updates ; do
 	pushd ${d} > /dev/null
 	if [[ -f eupdate.noupdate ]] || [[ -f eupdate.skip ]] ; then
-		echo "skipping $(pwd)"
+		echo "skipping $d"
 	else
 		eupdate >& /var/tmp/eupdate.out.$$
 		ret=$?
 		case $ret in
 			-1)
-				echo "$(pwd) has errors"
+				echo "$d has errors"
 				cp /var/tmp/eupdate.out.$$ eupdate.errors
 			;;
 			1)
-				echo "$(pwd) has updates"
+				echo "$d has updates"
 				cp /var/tmp/eupdate.out.$$ eupdate.updates
 			;;
 		esac
