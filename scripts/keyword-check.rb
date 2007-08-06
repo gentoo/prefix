be603a7e9f22e88aecc9873801b93ff54c5c9c4a
Fabian Groffen <grobian@gentoo.org>
2007-08-06 21:04:13 +0000

    spaces -> tabs

--- scripts/keyword-check.rb
+++ scripts/keyword-check.rb
@@ -5,32 +5,34 @@ require 'pathname'
 
 lines = Pathname.new( 'profiles/arch.list' ).readlines
 allowed = lines.collect {|line| line.chomp }.reject {|line|
-  line.slice( 0, 1 ) == '#' or line.empty? or line == 'prefix'
+	line.slice( 0, 1 ) == '#' or line.empty? or line == 'prefix'
 }
 
 Pathname.new( '.' ).find {|file| 
-  next unless file.fnmatch? '*.ebuild'
-  file.readlines.each {|line|
-    unless line.slice( 0, 9 ) == 'KEYWORDS='
-      next
-    else
-      kws = line.chomp.slice( 10..-2 )
-      next if kws.empty?
-      forbidden = Array.new
-      stable    = Array.new
-      kws.split.each {|kw|
-        # keywords are only allowed to start with a tilde for now
-        stable << kw if is_stable = kw.slice( 0, 1 ) != '~'
-        forbidden << kw unless allowed.include?(
-          is_stable ? kw : kw.slice( 1..-1)
-        )
-      }
-      if stable.any? or forbidden.any?
-        puts 'EBUILD    : %s' % [ file.dirname.dirname + file.basename ]
-        puts 'stable    : %s' % stable.join( " "  ) if stable.any?
-        puts 'forbidden : %s' % forbidden.join( " " ) if forbidden.any?
-        puts
-      end
-    end
-  }
+	next unless file.fnmatch? '*.ebuild'
+	file.readlines.each {|line|
+		unless line.slice( 0, 9 ) == 'KEYWORDS='
+			next
+		else
+			kws = line.chomp.slice( 10..-2 )
+			next if kws.empty?
+			forbidden = Array.new
+			stable    = Array.new
+			kws.split.each {|kw|
+				# keywords are only allowed to start with a tilde for now
+				stable << kw if is_stable = kw.slice( 0, 1 ) != '~'
+				forbidden << kw unless allowed.include?(
+					is_stable ? kw : kw.slice( 1..-1)
+				)
+			}
+			if stable.any? or forbidden.any?
+				puts 'EBUILD    : %s' % [ file.dirname.dirname + file.basename ]
+				puts 'stable    : %s' % stable.join( " "  ) if stable.any?
+				puts 'forbidden : %s' % forbidden.join( " " ) if forbidden.any?
+				puts
+			end
+		end
+	}
 }
+
+# vim: set ts=2 sw=2 noexpandtab:
