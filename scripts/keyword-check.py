2ddebfcf48387a12fe5c545beb3193ca1a48f588
Elias Pipping <pipping@gentoo.org>
2007-08-10 22:56:15 +0000

    add a rewrite of keyword-check in python for comparison

---
+++ scripts/keyword-check.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# Copyright Gentoo Foundation 2007
+
+import glob
+import fileinput
+import re
+
+archlist = []
+first = 1
+
+for arch in open( 'profiles/arch.list', 'r' ).readlines():
+	arch = arch.rstrip()
+	if arch != '' and arch != 'prefix' and not arch.startswith( '#' ):
+		archlist.append( arch )
+
+for file in glob.glob( '*/*/*.ebuild' ):
+	for line in fileinput.input( [file] ):
+		if line.startswith( 'KEYWORDS=' ):
+			forbidden=[]
+			stable=[]
+			for kw in re.split( '\s+', line.split( '"' )[1] ):
+				if kw == '': pass
+				else:
+					if not kw.startswith( '~' ) and not kw.startswith( '-' ):
+						stable.append( kw )
+					else:
+						kw=kw.lstrip( '~-' )
+					if not kw in archlist: stable.append( kw )
+			if len( stable ) != 0 or len( forbidden ) != 0:
+				if not first: print
+				else: first=0
+				print 'EBUILD    : %s' % re.sub( '/[^/]+/', '/', file )
+				print 'stable    : %s' % stable
+				print 'forbidden : %s' % forbidden
+
+# vim: set ts=4 sw=4 noexpandtab:
