abefe4e3e2f136f2bfb2dbe6e02e7a506128da81
Fabian Groffen <grobian@gentoo.org>
2007-08-12 09:46:23 +0000

    Also deal with magic appearing multiple times in a string.

--- scripts/pilots/jailer.c
+++ scripts/pilots/jailer.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 #include <strings.h>
+#include <errno.h>
 
-#define BUFSIZE 8096
+#define BUFSIZE 8095
 
-int main(int argc, char** argv) {
+/**
+ * Writes padding zero-bytes after the first encountered zero-byte.
+ * Returns padding if no zero-byte was seen, or 0 if padding was
+ * applied.
+ */
+size_t padonwrite(size_t padding, char *buf, size_t len, FILE *fout) {
+	char *z;
+	if (padding == 0 || (z = strchr(buf, '\0')) == NULL || z > buf + len) {
+		/* cheap case, nothing complicated to do here */
+		fwrite(buf, len, 1, fout);
+	} else {
+		/* found a zero-byte, insert padding */
+		fwrite(buf, z - buf, 1, fout);
+		/* now pad with zeros so we don't screw up
+		 * the positions in the file */
+		buf[0] = '\0';
+		while (padding-- > 0)
+			fwrite(buf, 1, 1, fout);
+		fwrite(z, len - (z - buf), 1, fout);
+	}
+
+	return(padding);
+}
+
+int main(int argc, char **argv) {
 	FILE *fin;
 	FILE *fout;
-	char* magic;
-	char* value;
-	char buf[BUFSIZE];
-	char* tmp;
+	char *magic;
+	char *value;
+	char buf[BUFSIZE + 1];
+	char *tmp;
 	size_t len;
-	size_t tlen;
 	size_t pos;
+	size_t padding;
 	size_t magiclen;
 	size_t valuelen;
 
@@ -21,61 +46,72 @@ int main(int argc, char** argv) {
 		return(-1);
 	}
 
-	fin      = fopen(argv[1], "r");
-	fout     = fopen(argv[2], "w");
 	magic    = argv[3];
 	value    = argv[4];
 	magiclen = strlen(magic);
 	valuelen = strlen(value);
 
+	if (magiclen < valuelen) {
+		fprintf(stderr, "value length (%zd) is bigger than "
+				"the magic length (%zd)\n", valuelen, magiclen);
+		return(-1);
+	}
+
+	fin      = fopen(argv[1], "r");
+	if (fin == NULL) {
+		fprintf(stderr, "unable to open %s: %s\n", argv[1], strerror(errno));
+		return(-1);
+	}
+
+	fout     = fopen(argv[2], "w");
+	if (fin == NULL) {
+		fprintf(stderr, "unable to open %s: %s\n", argv[2], strerror(errno));
+		return(-1);
+	}
+
+	/* make sure there is a trailing zero-byte, such that strstr and
+	 * strchr won't go out of bounds causing segfaults.  */
+	buf[BUFSIZE] = '\0';
+
 	pos = 0;
+	padding = 0;
 	while ((len = fread(buf + pos, 1, BUFSIZE - pos, fin)) != 0 || pos > 0) {
 		len += pos;
 		if ((tmp = strstr(buf, magic)) != NULL) {
 			if (tmp > buf + len) {
 				/* get out of here, we're done (seeing results in
 				 * garbage) */
-				fwrite(buf, len, 1, fout);
+				padonwrite(padding, buf, len, fout);
 				break;
 			} else if (tmp == buf) {
+printf("magic found here\n");
 				/* do some magic, overwrite it basically */
 				fwrite(value, valuelen, 1, fout);
-				/* find the end of the string */
-				pos = magiclen;
-				while (pos < len && buf[pos] != '\0')
-					pos++;
-				if (pos == len) {
-					/* alert for now */
-					fprintf(stderr, "couldn't find end of string within "
-							"block, resulting binary may be corrupt!\n");
-				}
-				fwrite(buf + magiclen, pos - magiclen, 1, fout);
-				/* now pad the remainder with zeros so we don't screw up
-				 * the positions in the file */
-				tlen = magiclen - valuelen;
-				buf[0] = '\0';
-				while (tlen-- > 0)
-					fwrite(buf, 1, 1, fout);
-				memmove(buf, buf + pos, len - pos);
-				pos = len - pos;
+				/* store what we need to correct */
+				padding += magiclen - valuelen;
+				/* move away the magic */
+				pos = len - magiclen;
+				memmove(buf, buf + magiclen, pos);
 				continue;
 			} else {
+printf("magic found\n");
 				/* move this bunch to the front */
 				pos = len - (tmp - buf);
 			}
 		} else {
+printf("no magic in block :(\n");
 			/* magic is not in here, but might just start at the end
 			 * missing it's last char, so move that */
 			if (len != BUFSIZE) {
 				/* last piece */
-				fwrite(buf, len, 1, fout);
+				padonwrite(padding, buf, len, fout);
 				break;
 			} else {
 				pos = magiclen - 1;
 				tmp = buf + (len - pos);
 			}
 		}
-		fwrite(buf, len - pos, 1, fout);
+		padding = padonwrite(padding, buf, len - pos, fout);
 		memmove(buf, tmp, pos);
 	}
 	fflush(fout);
