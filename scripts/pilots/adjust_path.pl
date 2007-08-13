a7ddd5e3a1e1d6e10224b66d5819ee3946ab9fd2
Elias Pipping <pipping@gentoo.org>
2007-08-13 02:34:23 +0000

    handle file i/o & exceptions, match newlines.  fully functional now.

--- scripts/pilots/adjust_path.pl
+++ scripts/pilots/adjust_path.pl
@@ -4,35 +4,33 @@
 use warnings;
 use strict;
 
-# if (scalar @ARGV != 4) {
-#   print 'usage: in-file out-file original-path new-path'
-# }
+die 'usage: in-file out-file original-path new-path' if scalar @ARGV != 4;
 
-my $string = "0123\0\0/Users/pipping/Gentoo/foo:/Users/pipping/Gentoo/bar\0\0/Users/pipping/Gentoo/bin:/Users/pipping/Gentoo/sbin:/Users/pipping/Gentoo/etc\0456";
-my $search = "/Users/pipping/Gentoo";
-my $replace = "/FOO";
+( my $infile, my $outfile, my $search, my $replace ) = @ARGV;
 
 if ( length( $search ) < length( $replace ) ) {
-  die "the new prefix is longer than the old prefix"
+  die "The new prefix is longer than the old one."
 }
 
-$_ = $string;
+undef $/;
+open( INFILE, "<$infile" ) or die "Could not open $infile for reading: $!";
+$_ = <INFILE>;
 
 my $olen = length();
 
 #print "OLD string: " . $_ . "\n";
 #print "OLD length: " . length() . "\n";
 
-while ( /\G.*?$search/g ) {
+while ( /\G(?:.|\n)*?$search/g ) {
   my $beg = pos() - length( $search );
   my $occurrences = 1;
   my @joints;
-  while ( /\G([^\0]*?)$search/ ) {
+  while ( /\G([^\0]*?)$search/m ) {
     $occurrences++;
     push @joints, $1;
     pos() += length( $1 ) + length( $search );
   }
-  /\G((?:.(?!$search))*?)(?=\0|$)/g;
+  /\G((?:.(?!$search))*?)(?=\0|$)/gm;
   push @joints, $1;
   my $localrep = '';
   for ( my $i = 0; $i < $occurrences; $i++ ) {
@@ -42,9 +40,11 @@ while ( /\G.*?$search/g ) {
     ( length( $search ) - length( $replace ) ) * $occurrences
   );
   my $len = pos() - $beg;
-  print 'replacing `' . substr( $_, $beg, $len ) . "`\n";
-  print 'with      `' . $localrep . ".\n";
+  #print 'replacing `' . substr( $_, $beg, $len ) . "`\n";
+  #print 'with      `' . $localrep . ".\n";
+  my $buffer = pos();
   substr( $_, $beg, $len ) = $localrep;
+  pos() = $buffer;
 }
 
 #print "NEW string: " . $_ . "\n";
@@ -52,4 +52,10 @@ while ( /\G.*?$search/g ) {
 
 my $nlen = length();
 
-die "the file size changed, this should not happen" if $nlen != $olen;
+if ($nlen != $olen) {
+  die 'The file size has changed, this should not happen. Nothing was written.'
+} else {
+  open( OUTFILE, ">$outfile" ) or die "Could not open $outfile for writing: $!";
+  print OUTFILE ( $_ );
+  close( OUTFILE )
+}
