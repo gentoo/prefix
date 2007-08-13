74086011a66d8c871934a0034b8250bd621420be
Elias Pipping <pipping@gentoo.org>
2007-08-13 05:48:25 +0000

    tabify, trivial changes

--- scripts/pilots/adjust_path.pl
+++ scripts/pilots/adjust_path.pl
@@ -9,7 +9,7 @@ die 'usage: in-file out-file original-path new-path' if scalar @ARGV != 4;
 ( my $infile, my $outfile, my $search, my $replace ) = @ARGV;
 
 if ( length( $search ) < length( $replace ) ) {
-  die "The new prefix is longer than the old one."
+	die "The new prefix is longer than the old one."
 }
 
 undef $/;
@@ -22,29 +22,29 @@ my $olen = length();
 #print "OLD length: " . length() . "\n";
 
 while ( /\G(?:.|\n)*?$search/g ) {
-  my $beg = pos() - length( $search );
-  my $occurrences = 1;
-  my @joints;
-  while ( /\G([^\0]*?)$search/m ) {
-    $occurrences++;
-    push @joints, $1;
-    pos() += length( $1 ) + length( $search );
-  }
-  /\G((?:.(?!$search))*?)(?=\0|$)/gm;
-  push @joints, $1;
-  my $localrep = '';
-  for ( my $i = 0; $i < $occurrences; $i++ ) {
-    $localrep .= $replace . $joints[$i];
-  }
-  $localrep .= "\0" x (
-    ( length( $search ) - length( $replace ) ) * $occurrences
-  );
-  my $len = pos() - $beg;
-  #print 'replacing `' . substr( $_, $beg, $len ) . "`\n";
-  #print 'with      `' . $localrep . ".\n";
-  my $buffer = pos();
-  substr( $_, $beg, $len ) = $localrep;
-  pos() = $buffer;
+	my $beg = pos() - length( $search );
+	my $occurrences = 1;
+	my @joints;
+	while ( /\G([^\0]*?)$search/m ) {
+		$occurrences++;
+		push @joints, $1;
+		pos() += length( $1 ) + length( $search );
+	}
+	/\G((?:.(?!$search))*?)(?=\0|$)/gm;
+	push @joints, $1;
+	my $localrep = '';
+	for ( my $i = 0; $i < $occurrences; $i++ ) {
+		$localrep .= $replace . $joints[$i];
+	}
+	$localrep .= "\0" x (
+		( length( $search ) - length( $replace ) ) * $occurrences
+	);
+	my $len = pos() - $beg;
+	#print 'replacing `' . substr( $_, $beg, $len ) . "`\n";
+	#print 'with      `' . $localrep . ".\n";
+	my $buffer = pos();
+	substr( $_, $beg, $len ) = $localrep;
+	pos() = $buffer;
 }
 
 #print "NEW string: " . $_ . "\n";
@@ -53,9 +53,11 @@ while ( /\G(?:.|\n)*?$search/g ) {
 my $nlen = length();
 
 if ($nlen != $olen) {
-  die 'The file size has changed, this should not happen. Nothing was written.'
-} else {
-  open( OUTFILE, ">$outfile" ) or die "Could not open $outfile for writing: $!";
-  print OUTFILE ( $_ );
-  close( OUTFILE )
+	die 'The file size has changed, this should not happen. Nothing was written'
 }
+
+open( OUTFILE, ">$outfile" ) or die "Could not open $outfile for writing: $!";
+print OUTFILE ( $_ );
+close( OUTFILE )
+
+# vim: set ts=4 sw=4 noexpandtab:
