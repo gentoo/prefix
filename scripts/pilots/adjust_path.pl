1bffb8518a970eab95b5c7dfd2103f775bff646a
Elias Pipping <pipping@gentoo.org>
2007-08-13 10:13:58 +0000

    trivial changes

--- scripts/pilots/adjust_path.pl
+++ scripts/pilots/adjust_path.pl
@@ -13,41 +13,41 @@ if ( length( $search ) < length( $replace ) ) {
 }
 
 # prepare regex library
-my $fmatch = qr/\G(?:.|\n)*?$search/o;
-my $cmatch = qr/\G(.*?)$search/o;
-my $tmatch = qr/\G(.*?)(?!$search)(?=\0?$)/o;
+my $imatch = qr/\G[^\0]*?$search/o;				# initial match
+my $cmatch = qr/\G(.*?)$search/o;				# continued match
+my $tmatch = qr/\G(.*?)(?!$search)(?=\0?$)/o;	# tail of match
 
 $/ = "\0";
 open( INFILE, "<$infile" ) or die "Could not open $infile for reading: $!";
 open( OUTFILE, ">$outfile" ) or die "Could not open $outfile for writing: $!";
 
 while (<INFILE>) {
-	while ( m/$fmatch/g ) {
+	while ( m/$imatch/g ) {
 		my $beg = pos() - length( $search );
 		my $occurrences = 1;
 		my @joints;
 		while ( m/$cmatch/ ) {
 			$occurrences++;
 			push @joints, $1;
-			pos() += length( $1 ) + length( $search );
+			pos() += length( $1 ) + length( $search )
 		}
 		m/$tmatch/g;
 		push @joints, $1;
 		my $localrep;
 		for ( my $i = 0; $i < $occurrences; $i++ ) {
-			$localrep .= $replace . $joints[$i];
+			$localrep .= $replace . $joints[$i]
 		}
 		$localrep .= "\0" x (
 			( length( $search ) - length( $replace ) ) * $occurrences
 		);
 		my $len = pos() - $beg;
-		print 'replacing `' . substr( $_, $beg, $len ) . "`\n";
-		print 'with      `' . $localrep . "\n";
+		print '< ' . substr( $_, $beg, $len ) . "\n";
+		print '> ' . $localrep . "\n";
 		my $buffer = pos();
 		substr( $_, $beg, $len ) = $localrep;
-		pos() = $buffer;
+		pos() = $buffer
 	}
-	print OUTFILE;
+	print OUTFILE
 }
 
 close( INFILE );
