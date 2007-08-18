697dfb8e06ed0bda536a3a966bf47b5307b43ce8
Elias Pipping <pipping@gentoo.org>
2007-08-18 06:53:18 +0000

    rename some variables, remove cruft

--- scripts/pilots/adjust_path.pl
+++ scripts/pilots/adjust_path.pl
@@ -6,7 +6,7 @@ use strict;
 
 die 'usage: in-file out-file original-path new-path' if scalar @ARGV != 4;
 
-( my $infile, my $outfile, my $search, my $replace ) = @ARGV;
+my ( $ifile, $ofile, $search, $replace ) = @ARGV;
 
 if ( length( $search ) < length( $replace ) ) {
 	die "The new prefix is longer than the old one."
@@ -18,10 +18,10 @@ my $cmatch = qr/\G(.*?)$search/o;				# continued match
 my $tmatch = qr/\G(.*?)(?!$search)(?=\0?$)/o;	# tail of match
 
 $/ = "\0";
-open( INFILE, "<$infile" ) or die "Could not open $infile for reading: $!";
-open( OUTFILE, ">$outfile" ) or die "Could not open $outfile for writing: $!";
+open( IFILE, "<$ifile" ) or die "Could not open $ifile for reading: $!";
+open( OFILE, ">$ofile" ) or die "Could not open $ofile for writing: $!";
 
-while (<INFILE>) {
+while (<IFILE>) {
 	while ( m/$imatch/g ) {
 		my $beg = pos() - length( $search );
 		my $occurrences = 1;
@@ -43,14 +43,14 @@ while (<INFILE>) {
 		my $len = pos() - $beg;
 		print '< ' . substr( $_, $beg, $len ) . "\n";
 		print '> ' . $localrep . "\n";
-		my $buffer = pos();
+		my $buf = pos();
 		substr( $_, $beg, $len ) = $localrep;
-		pos() = $buffer
+		pos() = $buf
 	}
-	print OUTFILE
+	print OFILE
 }
 
-close( INFILE );
-close( OUTFILE );
+close( IFILE );
+close( OFILE );
 
 # vim: set ts=4 sw=4 noexpandtab:
