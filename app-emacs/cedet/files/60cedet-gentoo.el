fb53c19cbe2c60862349cbcbf5e6d81f3e59f743
Elias Pipping <pipping@gentoo.org>
2007-09-27 11:00:11 +0000

    app-emacs/cedet: import & keyword ~x86-macos (Portage version: 2.2.00.7791-prefix/SVN/Darwin 8.10.1 i386)

---
+++ app-emacs/cedet/files/60cedet-gentoo.el
@@ -0,0 +1,8 @@
+
+;;; cedet site-lisp configuration
+
+(load "@SITELISP@/common/cedet" nil t)
+
+;; If you wish to customize CEDET, you will need to follow the
+;; directions in the INSTALL (installed in the documentation) file and
+;; customize your ~/.emacs /before/ site-gentoo is loaded.
