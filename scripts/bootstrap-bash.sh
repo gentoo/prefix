f657d4be49485c5284a9ce3574698e9f03d69ad5
Fabian Groffen <grobian@gentoo.org>
2007-10-03 16:06:28 +0000

    Add script to bootstrap a bash on FreeBSD, as without it bootstrapping needs to be completely rewritten.  Compiling bash is easier, and takes less longer.

---
+++ scripts/bootstrap-bash.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# FreeBSD bash installer
+#
+# FreeBSD comes with some POSIX (?) /bin/sh which doesn't eat most of
+# the stuff in the bootstrap-prefix script, among which the most
+# important part: dynamic function calling.  So, we need to bootstrap
+# bash outside the bootstrap script, which is the purpose of this
+# script.
+
+[ -z "$1" ] && exit -1
+
+cd "$1"
+mkdir bash-build
+cd bash-build
+
+fetch http://ftp.gnu.org/gnu/bash/bash-3.2.tar.gz
+gzip -d bash-3.2.tar.gz
+tar -xf bash-3.2.tar
+cd bash-3.2
+
+./configure --prefix="${1}"/usr --disable-nls
+make
+make install
