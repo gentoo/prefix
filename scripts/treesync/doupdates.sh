2b6712935008ab5fd1eefd0d2c43c8551b4dbc80
Fabian Groffen <grobian@gentoo.org>
2007-11-16 18:47:01 +0000

    Change the order such that profiles (package.mask) and eclass changes are done before the ebuild updates.

--- scripts/treesync/doupdates.sh
+++ scripts/treesync/doupdates.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+find profiles -name "eupdate.updates" -exec ./approve.sh '{}' \;
+find eclass -name "eupdate.updates" -exec ./approve.sh '{}' \;
+find licenses -name "eupdate.updates" -exec ./approve.sh '{}' \;
 find . -name "eupdate.updates" -exec ./approve.sh '{}' \;
