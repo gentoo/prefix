1807773e134c45325bd7db69bfc7db21dfa927d9
Fabian Groffen <grobian@gentoo.org>
2007-11-20 19:17:18 +0000

    Fix conf.d scripts being non-prefixed (Portage version: 2.2.00.8530-prefix/SVN/SunOS 5.10 i386)

--- gnustep-base/gnustep-make/files/gnustep-2.sh
+++ gnustep-base/gnustep-make/files/gnustep-2.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env sh
+# Copyright 1999-2007 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /var/cvsroot/gentoo-x86/gnustep-base/gnustep-make/files/gnustep-2.sh,v 1.2 2007/11/20 19:12:13 grobian Exp $
 
 # Test for an interactive shell
 case $- in
@@ -9,7 +12,7 @@ case $- in
 	;;
 esac
 	
-GNUSTEP_SYSTEM_TOOLS=/usr/GNUstep/System/Tools
+GNUSTEP_SYSTEM_TOOLS="@GENTOO_PORTAGE_EPREFIX@"/usr/GNUstep/System/Tools
 
 if [ -x ${GNUSTEP_SYSTEM_TOOLS}/make_services ]; then
     ${GNUSTEP_SYSTEM_TOOLS}/make_services
