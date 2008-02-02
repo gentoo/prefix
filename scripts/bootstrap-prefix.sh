8a5a2cba55ec2859542b9a9aed0ebb5556d434d3
Fabian Groffen <grobian@gentoo.org>
2008-02-02 17:33:07 +0000

    Use latest bootstrap image (popt issue) and latest portage

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -193,7 +193,7 @@ bootstrap_setup() {
 }
 
 bootstrap_tree() {
-	local def="20080130"
+	local def="20080202"
 	case ${CHOST} in
 		powerpc-ibm-aix*)            PV="${def}" ;;
 		i*86-apple-darwin8)          PV="${def}" ;;
@@ -269,7 +269,7 @@ bootstrap_startscript() {
 bootstrap_portage() {
 	# don't use "latest" here, as I want to have the bootstrap script to
 	# use a portage in a known "state"
-	PV="2.2.00.9233"
+	PV="2.2.00.9271"
 	A=prefix-portage-${PV}.tar.bz2
 	einfo "Bootstrapping ${A%-*}"
 		
