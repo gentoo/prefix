caae6504d5afd13ca64fc8f80a0e87337e4a9b52
Fabian Groffen <grobian@gentoo.org>
2008-02-22 21:44:46 +0000

    Use a snapshot of something similar to what I recently bootstrapped with on Solaris

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -183,7 +183,7 @@ bootstrap_setup() {
 }
 
 bootstrap_tree() {
-	local def="20080202"
+	local def="20080222"
 	case ${CHOST} in
 		powerpc-ibm-aix*)            PV="${def}" ;;
 		i*86-apple-darwin8)          PV="${def}" ;;
