00d2d98715eb885d0c43c59d0b9f60ca30398fe3
Fabian Groffen <grobian@gentoo.org>
2008-07-10 19:56:54 +0000

    bump snapshot to fix coreutils problem on RHEL4

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -198,7 +198,7 @@ bootstrap_setup() {
 }
 
 bootstrap_tree() {
-	local def="20080626"
+	local def="20080710"
 	case ${CHOST} in
 		powerpc-ibm-aix*)            PV="${def}" ;;
 		i*86-apple-darwin8)          PV="${def}" ;;
