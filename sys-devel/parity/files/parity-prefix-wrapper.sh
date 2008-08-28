ade39dc01b821d056ffd6a65075ca562ddd69187
Markus Duft <mduft@gentoo.org>
2008-08-28 07:49:49 +0000

    corrected mistakes in the wrapper script (Portage version: 2.2.00.11472-prefix/svn/Interix 6.0 x86)

--- sys-devel/parity/files/parity-prefix-wrapper.sh
+++ sys-devel/parity/files/parity-prefix-wrapper.sh
@@ -4,11 +4,10 @@
 # and/or linker call.
 
 compiler=$0
-exeext=@EXEEXT@
 
 case "${compiler}" in
-*-ld) compiler=parity.gnu.ld$exeext ;;
-*) compiler=parity.gnu.gcc$exeext ;;
+*-ld) compiler=parity.gnu.ld@EXEEXT@ ;;
+*) compiler=parity.gnu.gcc@EXEEXT@ ;;
 esac
 
 # parity is not very picky about getting options not required for
@@ -16,4 +15,7 @@ esac
 link_add="-L${EPREFIX}/lib -L${EPREFIX}/usr/lib -rpath ${EPREFIX}/lib -rpath ${EPREFIX}/usr/lib"
 comp_add="-I${EPREFIX}/include -I${EPREFIX}/usr/include"
 
-exec $compiler $com_add $link_add "$@"
+# options added at end of command line intentionally, to keep up
+# correct search orders for user given paths, and allow overriding
+# single files from other directories.
+exec $compiler "$@" $comp_add $link_add
