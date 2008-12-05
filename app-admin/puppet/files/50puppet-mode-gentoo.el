c692c2a8bc331fdfb6ea5b4e88b41d42c86282c9
Fabian Groffen <grobian@gentoo.org>
2008-12-05 09:50:18 +0000

    Add app-admin/puppet (Portage version: 2.2.00.12040-prefix/svn/SunOS 5.10 i386)

---
+++ app-admin/puppet/files/50puppet-mode-gentoo.el
@@ -0,0 +1,8 @@
+;;
+;; Setup puppet-mode for autoloading
+
+;;; puppet-mode site-lisp configuration
+
+(add-to-list 'load-path "@SITELISP@")
+(autoload 'puppet-mode "puppet-mode" "Major mode for editing puppet manifests")
+(add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
