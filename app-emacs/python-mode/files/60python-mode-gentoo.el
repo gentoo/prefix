48a88d0c96134285f1f953c2ce11e960aea73ac8
Jeremy Olexa <darkside@gentoo.org>
2008-12-10 18:42:09 +0000

    Import app-emacs/python-mode from main tree (Portage version: 2.2.00.12182-prefix/svn/Linux 2.6.9-55.ELsmp x86_64)

---
+++ app-emacs/python-mode/files/60python-mode-gentoo.el
@@ -0,0 +1,16 @@
+
+;;; python-mode site-lisp configuration
+
+(add-to-list 'load-path "@SITELISP@")
+
+(autoload 'python-mode "python-mode" "Python editing mode." t)
+(autoload 'jython-mode "python-mode" "Python editing mode." t)
+(autoload 'py-shell "python-mode" "Start an interactive Python interpreter in another window." t)
+(autoload 'doctest-mode "doctest-mode" "Editing mode for Python Doctest examples." t)
+(autoload 'py-complete "pycomplete" "Complete a symbol at point using Pymacs." t)
+
+(add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
+(add-to-list 'auto-mode-alist '("\\.doctest$" . doctest-mode))
+
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+(add-to-list 'interpreter-mode-alist '("jython" . jython-mode))
---
+++ app-emacs/python-mode/files/setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+setup(name="pycomplete", py_modules=["pycomplete"])
+
