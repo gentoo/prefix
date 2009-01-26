0441ecaae73e4a805fde725feda6e4ca8e24f2e6
Markus Duft <mduft@gentoo.org>
2009-01-26 09:05:58 +0000

    updated prefix wrapper script to handle library paths correctly (Portage version: 2.2.00.12528-prefix/svn/Interix x86)

--- sys-devel/parity/files/parity-prefix-wrapper.sh
+++ sys-devel/parity/files/parity-prefix-wrapper.sh
@@ -10,13 +10,38 @@ case "${compiler}" in
 *) compiler=parity.gnu.gcc@EXEEXT@ ;;
 esac
 
+opts=
+link_dirs=
+
+while test $# -gt 0; do
+	opt=$1
+
+	shift
+
+	case "${opt}" in
+	-L)
+		dir=$1
+		shift
+
+		link_dirs="${link_dirs} -L${dir}"
+		;;
+	-L*)
+		dir=${opt#-L}
+		link_dirs="${link_dirs} -L${dir}"
+		;;
+	*) ;;
+	esac
+
+	opts="${opts} ${opt}"
+done
+
 # parity is not very picky about getting options not required for
 # the current pass, so give them always.
-link_add="-cfg SysLibraryPaths=${EPREFIX}/lib -cfg SysLibraryPaths=${EPREFIX}/usr/lib -cfg RunPaths=${EPREFIX}/lib -cfg RunPaths=${EPREFIX}/usr/lib"
+link_add="-L${EPREFIX}/lib -L${EPREFIX}/usr/lib -rpath ${EPREFIX}/lib -rpath ${EPREFIX}/usr/lib"
 comp_add="-I${EPREFIX}/include -I${EPREFIX}/usr/include"
 
 # options added at end of command line intentionally, to keep up
 # correct search orders for user given paths, and allow overriding
 # single files from other directories. Cannot be done with link
 # relevant dirs, since library lookup is done in the correct order.
-exec $compiler $link_add "$@" $comp_add
+exec $compiler $link_dirs $link_add $opts $comp_add
