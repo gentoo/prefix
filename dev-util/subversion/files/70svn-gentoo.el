cbc80fbb5b5a51e4ff8bcef50536694afb43a7e9
Fabian Groffen <grobian@gentoo.org>
2009-04-11 10:05:11 +0000

    Semi auto-sync (manually editted for 1m 54s) -- dev-util/subversion (2009-04-11 11:23:50.503843304 +0200) (Portage version: 2.2.00.13286-prefix/svn/SunOS i386)

--- dev-util/subversion/files/70svn-gentoo.el
+++ dev-util/subversion/files/70svn-gentoo.el
@@ -7,5 +7,7 @@
 (add-to-list 'vc-handled-backends 'SVN)
 
 (defalias 'svn-examine 'svn-status)
+(autoload 'svn-status "dsvn" "Run `svn status'." t)
+(autoload 'svn-update "dsvn" "Run `svn update'." t)
 (autoload 'svn-status "psvn"
   "Examine the status of Subversion working copy in directory DIR." t)
