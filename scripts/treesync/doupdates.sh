098d6f2d852f5b33eb013bc415266cdfec1fa846
Fabian Groffen <grobian@gentoo.org>
2009-06-19 19:16:47 +0000

    disable old cruft by default

--- scripts/treesync/doupdates.sh
+++ scripts/treesync/doupdates.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 find profiles -name "eupdate.updates" -exec ./approve.sh '{}' \;
-( export PTREEDIR=$(pwd -P) && cd profiles && eupdate ChangeLog categories info_pkgs info_vars license_groups package.mask thirdpartymirrors profiles.desc use.desc use.local.desc && sed -i -e 's/^#prefix#//' profiles.desc && $SHELL)
+( export PTREEDIR=$(pwd -P) && cd profiles && eupdate ChangeLog categories info_pkgs info_vars license_groups package.mask thirdpartymirrors profiles.desc use.desc use.local.desc && sed -i -e 's/^#prefix#//' -e '/^[^#].*default-.*$/s/^/#/' profiles.desc && $SHELL )
 find eclass -name "eupdate.updates" -exec ./approve.sh '{}' \;
 find licenses -name "eupdate.updates" -exec ./approve.sh '{}' \;
 find . -name "eupdate.updates" -exec ./approve.sh '{}' \;
