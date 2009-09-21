7fe9d4664753b5db1b871d0707fb489f1ad707b6
Fabian Groffen <grobian@gentoo.org>
2009-09-21 20:58:04 +0000

    Semi auto-sync (manually editted for 0m 44s) -- app-emacs/gentoo-syntax (2009-09-21 19:04:14.047925119 +0200) (Portage version: 2.2.00.14200-prefix/svn/SunOS i386)

--- app-emacs/gentoo-syntax/files/50gentoo-syntax-gentoo.el
+++ app-emacs/gentoo-syntax/files/50gentoo-syntax-gentoo.el
@@ -3,7 +3,8 @@
   "Major mode for Portage .ebuild and .eclass files." t)
 (autoload 'eselect-mode "gentoo-syntax" "Major mode for .eselect files." t)
 
-(add-to-list 'auto-mode-alist '("\\.\\(ebuild\\|eclass\\)\\'" . ebuild-mode))
+(add-to-list 'auto-mode-alist
+	     '("\\.\\(ebuild\\|eclass\\|eblit\\)\\'" . ebuild-mode))
 (add-to-list 'auto-mode-alist '("\\.eselect\\'" . eselect-mode))
 (add-to-list 'interpreter-mode-alist '("runscript" . sh-mode))
 (modify-coding-system-alist 'file "\\.\\(ebuild\\|eclass\\)\\'" 'utf-8)
