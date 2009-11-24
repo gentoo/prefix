48ffd6539be889b52973da0960f3101435e5570e
Jeremy Olexa <darkside@gentoo.org>
2009-11-24 20:55:48 +0000

    Fauli moved app-emacs/autoconf-mode, app-emacs/bbdb, & app-emacs/cedet to gx86. We also don't need the static skel's in our repo

--- app-emacs/bbdb/files/50bbdb-gentoo.el
+++
@@ -1,8 +0,0 @@
-
-;;; bbdb site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(add-to-list 'load-path "@SITELISP@/bits")
-(require 'bbdb)
-(bbdb-initialize)
-
--- app-emacs/cedet/files/60cedet-gentoo.el
+++
@@ -1,8 +0,0 @@
-
-;;; cedet site-lisp configuration
-
-(load "@SITELISP@/common/cedet" nil t)
-
-;; If you wish to customize CEDET, you will need to follow the
-;; directions in the INSTALL (installed in the documentation) file and
-;; customize your ~/.emacs /before/ site-gentoo is loaded.
