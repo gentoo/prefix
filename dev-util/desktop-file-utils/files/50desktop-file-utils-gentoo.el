5efe120c3d8dcfc41413a894c2fffedaacee3332
Fabian Groffen <grobian@gentoo.org>
2010-01-16 10:04:41 +0000

    remove dev-util/desktop-file-utils, Fauli transferred keywords, but apparently forgot to remove from Prefix tree

--- dev-util/desktop-file-utils/files/50desktop-file-utils-gentoo.el
+++
@@ -1,7 +0,0 @@
-
-;; desktop-file-utils site initialisation
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'desktop-entry-mode "desktop-entry-mode" "Desktop Entry mode" t)
-(add-to-list 'auto-mode-alist
- '("\\.desktop\\(\\.in\\)?$" . desktop-entry-mode))
-(add-hook 'desktop-entry-mode-hook 'turn-on-font-lock)
