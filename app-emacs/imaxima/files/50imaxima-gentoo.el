317441852b2729968943fcaf20847a62aaaf2543
Fabian Groffen <grobian@gentoo.org>
2010-01-31 12:37:48 +0000

    ulm moved app-emacs/imaxima to gx86, games-misc/fortune-mod-dune has been restored again

--- app-emacs/imaxima/files/50imaxima-gentoo.el
+++
@@ -1,6 +0,0 @@
-
-;;; imaxima site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'imaxima "imaxima" "Image support for Maxima." t)
-(autoload 'imath-mode "imath" "Interactive Math minor mode." t)
