e71108cd82215a6ea74129ec1b656b6961a4ddeb
Jeremy Olexa <darkside@gentoo.org>
2010-02-01 23:00:21 +0000

    ulm moved app-emacs/{elib,howm,navi2ch} to gx86

--- app-emacs/elib/files/50elib-gentoo.el
+++
@@ -1,4 +0,0 @@
-
-;;; elib site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
--- app-emacs/navi2ch/files/50navi2ch-gentoo.el
+++
@@ -1,5 +0,0 @@
-
-;;; navi2ch site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'navi2ch "navi2ch" "Navigator for 2ch for Emacs." t)
