c5b044f8ec097e1236dc016a20da2564214d72f6
Jeremy Olexa <darkside@gentoo.org>
2010-08-13 16:52:23 +0000

    Set FEATURES=-news until after the --sync step because it doesn't add value

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -299,8 +299,10 @@ HOSTCC='gcc -m64'
 		# shoving this USE line into make.defaults, we can ensure that the
 		# end-user always avoids circular deps while bootstrapping and it gets
 		# wiped after a --sync. Also simplifies bootstrapping instructions.
-		echo "USE=\"-berkdb -fortran -gdbm -nls -pcre -ssl -python bootstrap\"" >>\
-			"${profile}"/make.defaults
+		echo "USE=\"-berkdb -fortran -gdbm -nls -pcre -ssl -python bootstrap\"" >> "${profile}"/make.defaults
+		# and we don't need to spam the user about news until after a --sync
+		# because the tools aren't available to read the news item yet anyway.
+		FEATURES="${FEATURES} -news" >> "${profile}"/make.defaults
 		einfo "Your make.globals is prepared for your current bootstrap"
 	fi
 }
