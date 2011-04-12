2f7f4e82582ba8d7312da7fffbcfd5b9552660f1
Jonathan Callen <jcallen@gentoo.org>
2011-04-12 22:51:51 +0000

    moved to gx86: app-misc/gcal app-text/a2ps app-text/dvipdfm app-text/libpaper app-text/poppler-data app-text/sary app-text/wv dev-util/fhist sys-apps/most

--- app-text/a2ps/files/50a2ps-gentoo.el
+++
@@ -1,8 +0,0 @@
-
-;;; a2ps site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'a2ps-mode "a2ps" nil t)
-(autoload 'a2ps-buffer "a2ps-print" nil t)
-(autoload 'a2ps-region "a2ps-print" nil t)
-(add-to-list 'auto-mode-alist '("\\.a2ps\\'" . a2ps-mode))
