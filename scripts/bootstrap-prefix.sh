aa98bb66aac116a81c6aa0a69697e8a5318d4660
Fabian Groffen <grobian@gentoo.org>
2012-08-10 19:53:37 +0000

    stage1: add forgotten --version for patch

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -898,7 +898,7 @@ bootstrap_stage1() {
 		|| bootstrap_coreutils || return 1
 	[[ $(find --version 2>&1) == *GNU* ]] || bootstrap_findutils || return 1
 	[[ $(tar --version 2>&1) == *GNU* ]] || bootstrap_tar || return 1
-	[[ $(patch 2>&1) == *GNU* ]] || bootstrap_patch || return 1
+	[[ $(patch --version 2>&1) == *GNU* ]] || bootstrap_patch || return 1
 	[[ $(grep --version 2>&1) == *GNU* ]] || bootstrap_grep || return 1
 	[[ $(awk --version 2>&1) == *GNU* ]] || bootstrap_gawk || return 1
 	[[ $(bash --version 2>&1) == "GNU bash, version 4"* ]] \
