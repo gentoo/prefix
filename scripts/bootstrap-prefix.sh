233f299dc3c977cc95caa722642ad3cff679acd1
Fabian Groffen <grobian@gentoo.org>
2012-08-11 08:21:03 +0000

    stage1: awk on Solaris ignores --version and waits for input, so give it none

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -900,7 +900,7 @@ bootstrap_stage1() {
 	[[ $(tar --version 2>&1) == *GNU* ]] || bootstrap_tar || return 1
 	[[ $(patch --version 2>&1) == *GNU* ]] || bootstrap_patch || return 1
 	[[ $(grep --version 2>&1) == *GNU* ]] || bootstrap_grep || return 1
-	[[ $(awk --version 2>&1) == *GNU* ]] || bootstrap_gawk || return 1
+	[[ $(awk --version < /dev/null 2>&1) == *GNU* ]] || bootstrap_gawk || return 1
 	[[ $(bash --version 2>&1) == "GNU bash, version 4"* ]] \
 		|| bootstrap_bash || return 1
 	# important to have our own (non-flawed one) since Python and
