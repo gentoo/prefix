6706172d6262b599938230775464ccc91252f81e
Fabian Groffen <grobian@gentoo.org>
2013-07-24 09:59:15 +0000

    bump snapshots

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -372,7 +372,7 @@ do_tree() {
 }
 
 bootstrap_tree() {
-	local PV="20130324"
+	local PV="20130723"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
@@ -437,7 +437,7 @@ bootstrap_portage() {
 	# STABLE_PV that is known to work. Intended for power users only.
 	## It is critical that STABLE_PV is the lastest (non-masked) version that is
 	## included in the snapshot for bootstrap_tree.
-	STABLE_PV="2.2.01.21864"
+	STABLE_PV="2.2.01.22124"
 	PV="${TESTING_PV:-${STABLE_PV}}"
 	A=prefix-portage-${PV}.tar.bz2
 	einfo "Bootstrapping ${A%-*}"
