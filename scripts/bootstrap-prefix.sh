3bf726577e7c26723825ff97e73e9dfa0d6c0e4f
Ruud Koolen <redlizard@gentoo.org>
2014-03-06 20:41:06 +0100

    Renamed bsddb module in bootstrap python.

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -802,7 +802,7 @@ bootstrap_python() {
 	# just make sure Python won't find it
 	export HOME="${S}"
 
-	export PYTHON_DISABLE_MODULES="bsddb bsddb185 bz2 crypt _ctypes_test _curses _curses_panel dbm _elementtree gdbm _locale nis pyexpat readline _sqlite3 _tkinter"
+	export PYTHON_DISABLE_MODULES="_bsddb bsddb bsddb185 bz2 crypt _ctypes_test _curses _curses_panel dbm _elementtree gdbm _locale nis pyexpat readline _sqlite3 _tkinter"
 	export PYTHON_DISABLE_SSL=1
 	export OPT="${CFLAGS}"
 
