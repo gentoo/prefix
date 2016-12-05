879c56524060193d963923f793da2b86f6eee922
Michael Haubenwallner <michael.haubenwallner@ssi-schaefer.com>
2016-12-05 17:51:02 +0100

    bootstrap-prefix.sh: can just check for existence of /var/run/cygfork/

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -1624,24 +1624,7 @@ EOF
 	esac
 
 	if [[ ${CHOST} == *-cygwin* ]]; then
-		have_cygfork=false
 		if [[ -r /var/run/cygfork/. ]]; then
-			if [[ -e /var/run/cygfork/.needed ]] ||
-			   [[ -e /var/run/cygfork/.unchecked ]]
-			then
-				cygfork_clean=:
-			else
-			  > /var/run/cygfork/.needed
-			  cygfork_clean='rm -f /var/run/cygfork/.needed'
-			fi
-			if ( [[ -n $(ls /var/run/cygfork/*/*/*.exe 2>/dev/null) ]] )
-			then
-				have_cygfork=true
-			fi
-			${cygfork_clean}
-		fi
-
-		if ${have_cygfork}; then
 			cat << EOF
 
 Whoah there, I've found the working fork() in your Cygwin instance,
