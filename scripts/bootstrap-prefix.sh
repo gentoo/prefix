73ac91841555df947a9fcd1616c19e603bf821d7
Fabian Groffen <grobian@gentoo.org>
2017-04-06 08:55:50 +0200

    scripts/bootstrap-prefix: bump snapshot to a version uploaded on mirrors

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -511,7 +511,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20170330"
+	local PV="20170405"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
