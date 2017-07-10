475476c511b9ba0cc3cb963712d5d7acaa71e6c0
Fabian Groffen <grobian@gentoo.org>
2017-07-10 19:19:27 +0200

    scripts/bootstrap-prefix.sh: bump snapshot, bug #622290

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -510,7 +510,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20170405"
+	local PV="20170708"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
