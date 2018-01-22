gentoo/prefix

commit 1896ea58f9eef2916986ade1d46aa3e727fbccc7

dev-lang/python: fix bootstrapping on Darwin, bug #642590
Closes: https://bugs.gentoo.org/642590
Package-Manager: Portage-2.3.18-prefix, Repoman-2.3.6

dev-lang/python/python-2.7.14.ebuild
@@ -175,27 +175,27 @@ src_prepare() {
 }
 
 src_configure() {
-		# dbm module can be linked against berkdb or gdbm.
-		# Defaults to gdbm when both are enabled, #204343.
-		local disable
-		use berkdb   || use gdbm || disable+=" dbm"
-		use berkdb   || disable+=" _bsddb"
-		use gdbm     || disable+=" gdbm"
-		use ncurses  || disable+=" _curses _curses_panel"
-		use readline || disable+=" readline"
-		use sqlite   || disable+=" _sqlite3"
-		use ssl      || export PYTHON_DISABLE_SSL="1"
-		use tk       || disable+=" _tkinter"
-		use xml      || disable+=" _elementtree pyexpat" # _elementtree uses pyexpat.
-		[[ ${CHOST} == *64-apple-darwin* ]] && disable+=" Nav _Qt" # Carbon
-		[[ ${CHOST} == *-apple-darwin11 ]] && disable+=" _Fm _Qd _Qdoffs"
-		export PYTHON_DISABLE_MODULES="${disable}"
+	# dbm module can be linked against berkdb or gdbm.
+	# Defaults to gdbm when both are enabled, #204343.
+	local disable
+	use berkdb   || use gdbm || disable+=" dbm"
+	use berkdb   || disable+=" _bsddb"
+	use gdbm     || disable+=" gdbm"
+	use ncurses  || disable+=" _curses _curses_panel"
+	use readline || disable+=" readline"
+	use sqlite   || disable+=" _sqlite3"
+	use ssl      || export PYTHON_DISABLE_SSL="1"
+	use tk       || disable+=" _tkinter"
+	use xml      || disable+=" _elementtree pyexpat" # _elementtree uses pyexpat.
+	[[ ${CHOST} == *64-apple-darwin* ]] && disable+=" Nav _Qt" # Carbon
+	[[ ${CHOST} == *-apple-darwin11 ]] && disable+=" _Fm _Qd _Qdoffs"
+	export PYTHON_DISABLE_MODULES="${disable}"
 
-		if ! use xml; then
-			ewarn "You have configured Python without XML support."
-			ewarn "This is NOT a recommended configuration as you"
-			ewarn "may face problems parsing any XML documents."
-		fi
+	if ! use xml; then
+		ewarn "You have configured Python without XML support."
+		ewarn "This is NOT a recommended configuration as you"
+		ewarn "may face problems parsing any XML documents."
+	fi
 
 	if [[ -n "${PYTHON_DISABLE_MODULES}" ]]; then
 		einfo "Disabled modules: ${PYTHON_DISABLE_MODULES}"
@@ -228,7 +228,6 @@ src_configure() {
 		replace-flags -Os -O3  # comment #14
 	fi
 
-
 	if use prefix ; then
 		# for Python's setup.py not to do false assumptions (only looking in
 		# host paths) we need to make explicit where Prefix stuff is
@@ -236,10 +235,12 @@ src_configure() {
 		append-ldflags -L"${EPREFIX}"/$(get_libdir)
 		append-ldflags -L"${EPREFIX}"/usr/$(get_libdir)
 		# fix compilation on some 64-bits Linux hosts, #381163, #473520
-		for hostlibdir in /usr/lib32 /usr/lib64 /usr/lib /lib32 /lib64; do
-			[[ -d ${hostlibdir} ]] || continue
-			append-ldflags -L${hostlibdir}
-		done
+		if use amd64-linux ; then
+			for hostlibdir in /usr/lib32 /usr/lib64 /usr/lib /lib32 /lib64; do
+				[[ -d ${hostlibdir} ]] || continue
+				append-ldflags -L${hostlibdir}
+			done
+		fi
 		# Have to move $(CPPFLAGS) to before $(CFLAGS) to ensure that
 		# local include paths - set in $(CPPFLAGS) - are searched first.
 		sed -i -e "/^PY_CFLAGS[ \\t]*=/s,\\\$(CFLAGS)[ \\t]*\\\$(CPPFLAGS),\$(CPPFLAGS) \$(CFLAGS)," Makefile.pre.in || die

dev-lang/python/python-2.7.12.ebuild
@@ -237,10 +237,12 @@ src_configure() {
 		append-ldflags -L"${EPREFIX}"/$(get_libdir)
 		append-ldflags -L"${EPREFIX}"/usr/$(get_libdir)
 		# fix compilation on some 64-bits Linux hosts, #381163, #473520
-		for hostlibdir in /usr/lib32 /usr/lib64 /usr/lib /lib32 /lib64; do
-			[[ -d ${hostlibdir} ]] || continue
-			append-ldflags -L${hostlibdir}
-		done
+		if use amd64-linux ; then
+			for hostlibdir in /usr/lib32 /usr/lib64 /usr/lib /lib32 /lib64; do
+				[[ -d ${hostlibdir} ]] || continue
+				append-ldflags -L${hostlibdir}
+			done
+		fi
 		# Have to move $(CPPFLAGS) to before $(CFLAGS) to ensure that
 		# local include paths - set in $(CPPFLAGS) - are searched first.
 		sed -i -e "/^PY_CFLAGS[ \\t]*=/s,\\\$(CFLAGS)[ \\t]*\\\$(CPPFLAGS),\$(CPPFLAGS) \$(CFLAGS)," Makefile.pre.in || die

commit f29dafd086fdc9bc1cc505d9ac8fe849d65bcb8d

dev-lang/python: can use elibc_glibc, not amd64-linux
This must have been broken since
https://gitweb.gentoo.org/repo/proj/prefix.git/commit/?id=1896ea58f9eef2916986ade1d46aa3e727fbccc7

Bug: https://bugs.gentoo.org/381163
Bug: https://bugs.gentoo.org/473520
Package-Manager: Portage-2.3.19, Repoman-2.3.6

dev-lang/python/Manifest
@@ -1,5 +1,5 @@
-DIST 7be648659ef46f33db6913ca0ca5a809219d5629.zip 9199 SHA256 f813f6f9437cb152824670e46b9c5d4235d29b8b433b40d74bd936a8e540828b SHA512 cabc21d2d7379daf1f4bafe7ab00b70e5b4f707d3102496b96ff0dfdced26347097ad771c69e0b9488b7c54b1ef36f9bc019bee72f1ffcbb55dcd0e71095edfe WHIRLPOOL 2c0a08deb24940c58c9e524d47bbadab6706fb0873e5899fe98a41af0b9ab07ed9ca7b03ac57c2aa199572e347b5c500371a9ef63fdc2201043595a227cae0ad
-DIST 8ee84829be03c2a1aa74f0f095c994c1f43f4688.zip 14239 SHA256 811b6fe82630a04a44a6d16074af76964e7e4005136ba479647eb741639099da SHA512 2f3ada7ff2bd97f90e3b972b681624ddf45c6d1db8d5e89f1313b4693db240a3c4e27c84bd2cf8ff71d5fb276b381ca6a3b8a6945f8db53ff3496cc4cc0b1d62 WHIRLPOOL 73eba606051ed1232de22965f0879cd2dc3895d2d280a3e355581be788adf1af20e96c7eb725af2e71df1d022e6b208ab2ffe70515693536d6464f033224cd14
+DIST 7be648659ef46f33db6913ca0ca5a809219d5629.zip 9199 BLAKE2B 85803bc1677bd16650cb5fc4a2c54fdac0228326f25cfba4fcd098ac9d689312d3e0dde6fca57f720d5235361d6462461cf018325c23fda9139e0f1683055aff SHA512 cabc21d2d7379daf1f4bafe7ab00b70e5b4f707d3102496b96ff0dfdced26347097ad771c69e0b9488b7c54b1ef36f9bc019bee72f1ffcbb55dcd0e71095edfe
+DIST 8ee84829be03c2a1aa74f0f095c994c1f43f4688.zip 14239 BLAKE2B 3dd91f83e5886cb9ba5c1fd9f3da38ab521db70645fa187752cd784919d523d9bf59b61223ea1a455b6d9002ba88dac5bd834c8e943b121db84e30bbb0f65204 SHA512 2f3ada7ff2bd97f90e3b972b681624ddf45c6d1db8d5e89f1313b4693db240a3c4e27c84bd2cf8ff71d5fb276b381ca6a3b8a6945f8db53ff3496cc4cc0b1d62
 DIST Python-2.7.12.tar.xz 12390820 BLAKE2B 97446bca10005277cf1a92ce7fc9e847d5df93f656a6a3b66e9f064d1c2ba35d5a8b420aaa64b92247d6a3fcd3852876450faebc466c7a8e841fd0c2e62fd306 SHA512 6ddbbce47cc49597433d98ca05c2f62f07ed1070807b645602a8e9e9b996adc6fa66fa20a33cd7d23d4e7e925e25071d7301d288149fbe4e8c5f06d5438dda1f
 DIST Python-2.7.14.tar.xz 12576112 BLAKE2B e03c4134cfdd88678acc26f383130a598ce4de908ab6bb9ee00e9235de3e0fcb9e653fec86f39db64fb895ef797d33736524655c5b94fab2cde2544b966b2cfb SHA512 78310b0be6388ffa15f29a80afb9ab3c03a572cb094e9da00cfe391afadb51696e41f592eb658d6a31a2f422fdac8a55214a382cbb8cfb43d4a127d5b35ea7f9
 DIST Python-3.4.5.tar.xz 14516820 BLAKE2B f9ddabd2df2241089d07756d3a8ba1c70096bee60d7cdef84041edbb20c488c3a573762441a4b7f543dbc0ce403ce84ff7d02e7d25699917bd014ab96deead85 SHA512 b548a5338d141b3086dea372fa2cc245ba5350ee9f0d1304ef6222dc45380b66d380b092e547d54206d56ca68563b95036f445f97f72bb0ef1d48f81a74a3d4c

dev-lang/python/python-2.7.14.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2017 Gentoo Foundation
+# Copyright 1999-2018 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI="5"
@@ -244,8 +244,8 @@ src_configure() {
 		append-cppflags -I"${EPREFIX}"/usr/include
 		append-ldflags -L"${EPREFIX}"/$(get_libdir)
 		append-ldflags -L"${EPREFIX}"/usr/$(get_libdir)
-		# fix compilation on some 64-bits Linux hosts, #381163, #473520
-		if use amd64-linux ; then
+		# fix compilation on some Linux hosts, #381163, #473520
+		if use elibc_glibc ; then
 			for hostlibdir in /usr/lib32 /usr/lib64 /usr/lib /lib32 /lib64; do
 				[[ -d ${hostlibdir} ]] || continue
 				append-ldflags -L${hostlibdir}