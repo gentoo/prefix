57ab1106d40685de40ea05113233f0411cbf647d
Fabian Groffen <grobian@gentoo.org>
2018-02-09 09:15:21 +0100

    scripts/bootstrap-prefix: bump snapshot for openssl sync, bug #647006
    Closes: https://bugs.gentoo.org/647006

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -521,7 +521,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20180206"
+	local PV="20180208"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
