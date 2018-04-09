5432992f824454bce5266a2b99a1bb275823fcff
Fabian Groffen <grobian@gentoo.org>
2018-04-09 09:38:06 +0200

    scripts/bootstrap-prefix: bump snapshot, adds ppc-macos support

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -538,7 +538,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20180219"
+	local PV="20180408"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
