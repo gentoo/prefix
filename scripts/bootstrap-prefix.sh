732e197aabcb3cb107e5d719ff0b2e2597821d0f
Michael Haubenwallner <haubi@gentoo.org>
2019-03-04 14:59:09 +0100

    bump stage1 m4-1.4.18, may work with glibc-2.28
    Signed-off-by: Michael Haubenwallner <haubi@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -1193,7 +1193,10 @@ bootstrap_bison() {
 }
 
 bootstrap_m4() {
-	bootstrap_gnu m4 1.4.17 || bootstrap_gnu m4 1.4.16 || bootstrap_gnu m4 1.4.15
+	bootstrap_gnu m4 1.4.18 ||
+	bootstrap_gnu m4 1.4.17 ||
+	bootstrap_gnu m4 1.4.16 ||
+	bootstrap_gnu m4 1.4.15
 }
 
 bootstrap_gzip() {
