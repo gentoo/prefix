2185ae8dc748888b8f86221018b77d4aac8770e8
Fabian Groffen <grobian@gentoo.org>
2020-11-21 14:43:59 +0100

    scripts/bootstrap-prefix: use latest portage which matches snapshot
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -660,8 +660,8 @@ bootstrap_portage() {
 	# STABLE_PV that is known to work. Intended for power users only.
 	## It is critical that STABLE_PV is the lastest (non-masked) version that is
 	## included in the snapshot for bootstrap_tree.
-	STABLE_PV="2.3.100"
-	[[ ${TESTING_PV} == latest ]] && TESTING_PV="2.3.100"
+	STABLE_PV="3.0.8"
+	[[ ${TESTING_PV} == latest ]] && TESTING_PV="3.0.8"
 	PV="${TESTING_PV:-${STABLE_PV}}"
 	A=prefix-portage-${PV}.tar.bz2
 	einfo "Bootstrapping ${A%-*}"
