41a349e4c2d23aee41ae0cf8c30a9412bf1e1039
Fabian Groffen <grobian@gentoo.org>
2020-11-24 09:46:08 +0100

    scripts/bootstrap-prefix: bump bootstrap tree
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -593,7 +593,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20201120"
+	local PV="20201123"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
