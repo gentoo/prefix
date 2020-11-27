2fda1225f366f1ec1ce4d0ec4f15e5376dc9a572
Fabian Groffen <grobian@gentoo.org>
2020-11-27 11:32:47 +0100

    scripts/bootstrap-prefix: refresh snapshot for latest fixes
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -595,7 +595,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20201124"
+	local PV="20201126"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
