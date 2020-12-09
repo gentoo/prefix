0c3888b00fe6ecbe61abac0c32f1879454f6c743
Fabian Groffen <grobian@gentoo.org>
2020-12-09 16:55:43 +0100

    scripts/bootstrap-prefix: bump snapshot for gettext fix
    should fix gettext-0.21 failure in stage3 on Darwin
    
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -598,7 +598,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20201207"
+	local PV="20201208"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
