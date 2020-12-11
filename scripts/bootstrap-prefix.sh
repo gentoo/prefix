05ecbecb5e3633e2ae35c7784151227eb65ef5d6
Fabian Groffen <grobian@gentoo.org>
2020-12-11 09:00:57 +0100

    scripts/bootstrap-prefix: bump snapshot for Darwin and Solaris fixes
    - Darwin 9: fix Python 3.8 compilation
    - Solaris: fix bootstrap with Solaris nm (due to toolchain.eclass
      forcing NM=nm)
    
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -603,7 +603,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20201209"
+	local PV="20201210"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
