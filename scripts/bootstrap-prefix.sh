889898a2eac57b319d3e83da4f64b055b0b2aaef
Fabian Groffen <grobian@gentoo.org>
2020-12-12 10:06:16 +0100

    scripts/bootstap-prefix: bump snapshot for python-3.8.6 emerge fail
    this should fix darwin bootstraps again
    
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -603,7 +603,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20201210"
+	local PV="20201211"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
