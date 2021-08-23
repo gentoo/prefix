369152c566d4b0efe6dec6eee19cd6655c19d1b1
Fabian Groffen <grobian@gentoo.org>
2021-08-23 08:18:01 +0200

    scripts/bootstrap-prefix: bump snapshot for unmasked findutils
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -543,7 +543,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20210821"
+	local PV="20210822"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
