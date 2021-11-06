41dbf29061887de65341151ac31f28eaa77f7b45
Fabian Groffen <grobian@gentoo.org>
2021-11-06 09:39:41 +0100

    scripts/bootstrap-prefix: bump snapshot for Monterey
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -571,7 +571,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20211014"
+	local PV="20211105"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
