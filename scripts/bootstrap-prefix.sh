c7f2b0e464e4059bef08ea45faaf276e16695a07
Fabian Groffen <grobian@gentoo.org>
2022-01-17 08:15:53 +0100

    scripts/bootstrap-prefix: bump snapshot and portage version
    Bug: https://bugs.gentoo.org/831026
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -600,7 +600,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20211227"
+	local PV="20220116"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
@@ -667,8 +667,8 @@ bootstrap_portage() {
 	# STABLE_PV that is known to work. Intended for power users only.
 	## It is critical that STABLE_PV is the lastest (non-masked) version that is
 	## included in the snapshot for bootstrap_tree.
-	STABLE_PV="3.0.21"
-	[[ ${TESTING_PV} == latest ]] && TESTING_PV="3.0.21"
+	STABLE_PV="3.0.30.1"
+	[[ ${TESTING_PV} == latest ]] && TESTING_PV="3.0.30.1"
 	PV="${TESTING_PV:-${STABLE_PV}}"
 	A=prefix-portage-${PV}.tar.bz2
 	einfo "Bootstrapping ${A%.tar.*}"
