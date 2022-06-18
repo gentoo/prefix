77970d2b6d1d4fc05b68252a0ede211800be067c
Fabian Groffen <grobian@gentoo.org>
2022-06-18 09:01:04 +0200

    scripts/bootstrap-prefix: bump snapshot for gcc-12.1.0
    Our GCC 12.1 snapshot is prepped for Apple M1 support, preparing the way
    for a full arm64-darwin bootstrap.
    
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -611,7 +611,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20220116"
+	local PV="20220616"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
