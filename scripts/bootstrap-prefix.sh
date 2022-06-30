38898757029ed90d486d2dae6f0febf355bc9fd9
Sam James <sam@gentoo.org>
2022-06-30 19:14:41 +0000

    Revert "bootstrap-prefix.sh: bump snapshot for autotools.eclass (and distutils-r1) fix"
    I'd checked the snapshot was there but not the other prefix-overlay
    tarball it fetches.
    
    This reverts commit fe3475aa2bf9ef0c965909f0c846aa7b7a75cd04.
    
    Signed-off-by: Sam James <sam@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -618,7 +618,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20220629"
+	local PV="20220616"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
