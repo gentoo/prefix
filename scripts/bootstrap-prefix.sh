gentoo/prefix

commit fd1c9f9b6b838173e6f7df0cb8cc5114e55f6c56

scripts/bootstrap-prefix: bump bootstrap python to 3.10 to match tree
There is a problem which makes it hard to upgrade Python without
transition in place (addressed by
c310e44692ad91777c71394dda97cd1f33f72589?), so just use a bootstrap
Python that's equal to the version installed by the tree to avoid this
problem.

While at it, drop arm64-macos hack, since it's no longer necessary.

Bug: https://bugs.gentoo.org/778014
Signed-off-by: Fabian Groffen <grobian@gentoo.org>

scripts/bootstrap-prefix.sh
@@ -746,7 +746,7 @@ bootstrap_portage() {
 
 	local tmpportdir=${ROOT}/tmp/${PORTDIR#${ROOT}}
 	[[ -e "${tmpportdir}" ]] || ln -s "${PORTDIR}" "${tmpportdir}"
-	for d in "${ROOT}"/tmp/usr/lib/python?.?; do
+	for d in "${ROOT}"/tmp/usr/lib/python${PYTHONMAJMIN}; do
 		[[ -e ${d}/portage ]] || ln -s "${ROOT}"/tmp/usr/lib/portage/lib/portage ${d}/portage
 		[[ -e ${d}/_emerge ]] || ln -s "${ROOT}"/tmp/usr/lib/portage/lib/_emerge ${d}/_emerge
 	done
@@ -1028,9 +1028,9 @@ bootstrap_gnu() {
 	einfo "${A%.tar.*} successfully bootstrapped"
 }
 
-PYTHONMAJMIN=3.9   # keep this number in line with PV below for stage1,2
+PYTHONMAJMIN=3.10   # keep this number in line with PV below for stage1,2
 bootstrap_python() {
-	PV=3.9.13
+	PV=3.10.4
 	A=Python-${PV}.tar.xz
 	einfo "Bootstrapping ${A%.tar.*}"
 
@@ -1118,12 +1118,6 @@ bootstrap_python() {
 		# fixup thread id detection
 		efetch "https://dev.gentoo.org/~sam/distfiles/dev-lang/python/python-3.9.6-darwin9_pthreadid.patch"
 		patch -p1 < "${DISTDIR}"/python-3.9.6-darwin9_pthreadid.patch
-		;;
-	(arm64-*-darwin*)
-		# Teach Python a new trick (arm64)
-		sed -i \
-			-e "/Unexpected output of 'arch' on OSX/d" \
-			configure
 		;;
 	(*-openbsd*)
 		# OpenBSD is not a multilib system

commit 8c14a3e911815124539ad2925e0c8f9ff59deed8

Fix prefix python bootstrap for cygwin
Upstream cygwin doesn't ship python 3.10 yet, and the patches in the 3.9
tarball don't apply cleanly.
As a consequence, cygwin bootstrap has been broken since fd1c9f9b6b838173e6f7df0cb8cc5114e55f6c56.
Fix this by using 3.9 to bootstrap for cygwin only.

Bug: https://bugs.gentoo.org/852665

Signed-off-by: Peter Foley <pefoley2@pefoley.com>

scripts/bootstrap-prefix.sh
@@ -997,9 +997,18 @@ bootstrap_gnu() {
 	einfo "${A%.tar.*} successfully bootstrapped"
 }
 
+if [[ ${CHOST} == *-cygwin* ]] ; then
+PYTHONMAJMIN=3.9   # keep this number in line with PV below for stage1,2
+else
 PYTHONMAJMIN=3.10   # keep this number in line with PV below for stage1,2
+fi
+
 bootstrap_python() {
-	PV=3.10.4
+	if [[ ${CHOST} == *-cygwin* ]] ; then
+          PV=3.9.9
+        else
+	  PV=3.10.4
+        fi
 	A=Python-${PV}.tar.xz
 	einfo "Bootstrapping ${A%.tar.*}"
 