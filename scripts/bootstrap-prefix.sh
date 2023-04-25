c9f40c64294b16646560d39ae010fc18c6f28985
Fabian Groffen <grobian@gentoo.org>
2023-04-25 17:06:30 +0200

    scripts/bootstrap-prefix: bump bootstrap snapshot for Darwin (arm64) fixes
    Closes: https://bugs.gentoo.org/898610
    Bug: https://bugs.gentoo.org/886491
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -584,7 +584,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20221104"
+	local PV="20230424"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
