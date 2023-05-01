c3ad2b1bac41f2b3089658c549f72c1e823e0a41
Fabian Groffen <grobian@gentoo.org>
2023-05-01 08:28:06 +0200

    scripts/bootstrap-prefix: bump snapshot
    This version allowed me to bootstrap x64-macos on darwin17.
    
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -584,7 +584,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20230424"
+	local PV="20230429"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
