02c424a9b45035b690daf58088bc6db30220d9ba
Fabian Groffen <grobian@gentoo.org>
2023-07-08 07:57:10 +0200

    scripts/bootstrap-prefix: bump bootstrap snapshot for gettext/libintl
    Closes: https://bugs.gentoo.org/909041
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -563,7 +563,7 @@ do_tree() {
 bootstrap_tree() {
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
-	local PV="20230601"
+	local PV="20230707"
 	if [[ -n ${LATEST_TREE_YES} ]]; then
 		do_tree "${SNAPSHOT_URL}" portage-latest.tar.bz2
 	else
