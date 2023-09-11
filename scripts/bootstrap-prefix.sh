34fe8e37ce69ed9e459493a0ee56400e8e6f3212
Fabian Groffen <grobian@gentoo.org>
2023-09-11 21:51:45 +0200

    scripts/bootstrap-prefix: bump bootstrap snapshot
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -547,7 +547,7 @@ bootstrap_tree() {
 	#                      retain this comment and the line below to
 	#                      keep this snapshot around in the snapshots
 	# MKSNAPSHOT-ANCHOR -- directory of rsync slaves
-	local PV="20230709"
+	local PV="20230910"
 
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
