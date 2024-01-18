928d7d3bfb5e24f7ded38989820355508fb13a17
Fabian Groffen <grobian@gentoo.org>
2024-01-18 12:52:38 +0100

    scripts/bootstrap-prefix: use same Portage version as in bootstrap tree
    Use autotools-fixed version of Portage that is in the current bootstrap
    snapshot.  Perhaps this fixes the stage1 portage install problem
    reported on WSL.
    
    Bug: https://bugs.gentoo.org/910469
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -607,8 +607,8 @@ bootstrap_portage() {
 	# STABLE_PV that is known to work. Intended for power users only.
 	## It is critical that STABLE_PV is the lastest (non-masked) version that is
 	## included in the snapshot for bootstrap_tree.
-	STABLE_PV="3.0.49"
-	[[ ${TESTING_PV} == latest ]] && TESTING_PV="3.0.49"
+	STABLE_PV="3.0.56.1"
+	[[ ${TESTING_PV} == latest ]] && TESTING_PV="3.0.56.1"
 	PV="${TESTING_PV:-${STABLE_PV}}"
 	A=prefix-portage-${PV}.tar.bz2
 	einfo "Bootstrapping ${A%.tar.*}"
