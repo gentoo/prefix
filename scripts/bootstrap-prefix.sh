1152fbf41ae0e0272b8f77413d0dabb15e81ea59
Fabian Groffen <grobian@gentoo.org>
2024-04-06 09:13:54 +0200

    scripts/bootstrap-prefix: bump snapshot for Darwin fixes
    - binutils-config ld duplicate paths elimination
    - gcc math.h fix for darwin
    - openssl using --push-state --as-needed --pop-state LDFLAGS unconditional
    
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -573,7 +573,7 @@ bootstrap_tree() {
 	#                      retain this comment and the line below to
 	#                      keep this snapshot around in the snapshots
 	# MKSNAPSHOT-ANCHOR -- directory of rsync slaves
-	local PV="20240218"
+	local PV="20240405"
 
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
