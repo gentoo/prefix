64a78631d4bf682f8f95f87fe150accfa0d66222
Fabian Groffen <grobian@gentoo.org>
2024-04-10 08:59:22 +0200

    scripts/bootstrap-prefix: bump snapshot for Darwin Python fix
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -573,7 +573,7 @@ bootstrap_tree() {
 	#                      retain this comment and the line below to
 	#                      keep this snapshot around in the snapshots
 	# MKSNAPSHOT-ANCHOR -- directory of rsync slaves
-	local PV="20240407"
+	local PV="20240409"
 
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
