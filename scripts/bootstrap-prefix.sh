0c15f53abb6ee951a4381715e8cbdcc59e2e06cf
Fabian Groffen <grobian@gentoo.org>
2024-06-23 09:04:26 +0200

    scripts/bootstrap-prefix: bump snapshot for xz-utils fix
    We need 3f4b5440ef1e9b77b465dcaf0a0e104238ff6ed8 (from bug #934515) to
    make the bootstrap succeed on macOS/Darwin.
    
    Closes: https://bugs.gentoo.org/934757
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- scripts/bootstrap-prefix.sh
+++ scripts/bootstrap-prefix.sh
@@ -573,7 +573,7 @@ bootstrap_tree() {
 	#                      retain this comment and the line below to
 	#                      keep this snapshot around in the snapshots
 	# MKSNAPSHOT-ANCHOR -- directory of rsync slaves
-	local PV="20240617"
+	local PV="20240622"
 
 	# RAP uses the latest gentoo main repo snapshot to bootstrap.
 	is-rap && LATEST_TREE_YES=1
